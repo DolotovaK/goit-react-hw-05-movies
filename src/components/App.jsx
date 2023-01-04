18d7a599f11a554a6afb0a859847f8ee3270924d
Kateryna Dolotova <104764377+DolotovaK@users.noreply.github.com>
2023-01-04 23:20:54 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import { SharedLayout } from "./SharedLayout/SharedLayaut";
-import Home from "pages/Home";
-import MoviesPage from "pages/MoviesPage";
-import MovieDetails from "pages/MovieDetails";
-import Actors from "./Actors/Actors";
-import Reviews from "./Reviews/Reviews";
+// import Home from "pages/Home";
+// import MoviesPage from "pages/MoviesPage";
+// import MovieDetails from "pages/MovieDetails";
+// import Actors from "./Actors/Actors";
+// import Reviews from "./Reviews/Reviews";
+import { lazy } from "react";
+
+
+const Home = lazy(() => import('../pages/Home'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
+const MoviesPage = lazy(() => import('../pages/MoviesPage'));
+const Actors = lazy(() => import('./Actors/Actors'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
 
 export const App = () => {
   return (
